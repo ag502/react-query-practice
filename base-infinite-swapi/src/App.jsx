342d3d450c676e9eedd95185ad97634124628e3d
ag502 <ag502@naver.com>
2023-12-06 21:25:48 +0900

    ADD: useInfiniteQuery

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   return (
     <div className="App">
       <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
+      {/* <InfinitePeople /> */}
+      <InfiniteSpecies />
     </div>
   );
 }
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 const initialUrl = "https://swapi.dev/api/species/";
 const fetchUrl = async (url) => {
@@ -9,5 +10,26 @@ const fetchUrl = async (url) => {
 
 export function InfiniteSpecies() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, hasNextPage, isLoading, isError, isFetching, fetchNextPage } =
+    useInfiniteQuery({
+      queryKey: ["sw-species"],
+      queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+      getNextPageParam: (lastPage) => {
+        return lastPage.next || undefined;
+      },
+    });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {data.pages.map((pageData) => {
+        return pageData.results.map((species) => {
+          return <Species key={species.name} name={species.name} />;
+        });
+      })}
+    </InfiniteScroll>
+  );
 }
